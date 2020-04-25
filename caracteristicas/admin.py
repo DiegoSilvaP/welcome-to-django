6df15c6c6f0dfe464e13b31bd73beb5efb32b867
disilvap <alienware.matrix@gmail.com>
2020-04-25 18:12:30 -0500

    Mejoras en el admin

--- caracteristicas/admin.py
+++ caracteristicas/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Caracteristica
+from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(Caracteristica)
+class CaracteristicaAdmin(admin.ModelAdmin):
+    # Qué campos mostrar en la lista de registros
+    list_display = ('name','descripción_corta','created','updated','eliminar')
+    # Cuáles de esos campos pueden ser links para acceder a dicho registro
+    list_display_links=('name','descripción_corta')
+    # Qué campos se pueden utilizar para filtrar
+    list_filter = ('name','created','updated')
+    # A través de qué campos se puede buscar
+    search_fields = ('name','description')
+
+    # Creamos botón para eliminar y lo insertamos en cada fila
+    def eliminar(self, obj):
+        return format_html('<a class="deletelink" href="/admin/caracteristicas/caracteristica/{}/delete/">Eliminar</a>', obj.id)
+
+    # Personalizamos el cómo mostrar la descripción
+    def descripción_corta(self, obj):
+        # Se regresan 100 caracteres
+        return f"{obj.description[:100]}..."
+
+
+admin.site.register(Caracteristica, CaracteristicaAdmin)
