a1230a9097cb38104180c42c9e7dbda3bef72473
disilvap <alienware.matrix@gmail.com>
2020-04-25 21:56:22 -0500

    Mejoras en el admin

--- caracteristicas/admin.py
+++ caracteristicas/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Caracteristica
+from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(Caracteristica)
+class CaracteristicaAdmin(admin.ModelAdmin):
+    # Qué campos mostrar en la lista de registros
+    list_display = ('name','print_short_description','created','updated','remove_button')
+    # Cuáles de esos campos pueden ser links para acceder a dicho registro
+    list_display_links=('name','print_short_description')
+    # Qué campos se pueden utilizar para filtrar
+    list_filter = ('name','created','updated')
+    # A través de qué campos se puede buscar
+    search_fields = ('name','description')
+
+    # Creamos botón para eliminar y lo insertamos en cada fila
+    def remove_button(self, obj):
+        return format_html('<a class="deletelink" href="/admin/caracteristicas/caracteristica/{}/delete/">Eliminar</a>', obj.id)
+    remove_button.short_description = "Acción"
+
+    # Personalizamos el cómo mostrar la descripción
+    def print_short_description(self, obj):
+        # Se regresan 100 caracteres
+        return f"{obj.description[:100]}..."
+    print_short_description.short_description = "Descripción"
+    print_short_description.admin_order_field = "name"
+        
+
+
+admin.site.register(Caracteristica, CaracteristicaAdmin)
--- core/admin.py
+++ core/admin.py
@@ -4,14 +4,15 @@ from .models import Portada, Noticia, BotAplication
 # Register your models here.
 
 class NoticiaTabularInline(admin.TabularInline):
+# class NoticiaTabularInline(admin.StackedInline):
     model = Noticia
     extra =1
 
 class PortadaAdmin(admin.ModelAdmin):
     inlines = [NoticiaTabularInline]
-    list_display = ('title','subtitle','updated','created',)
-    search_fields = ['title','subtitle']
-    list_filter = ('title',)
+    list_display = ('title','subtitle','link','link_text','updated', 'created')
+    list_display_links=('title', 'subtitle')
+    list_editable = ('link','link_text')
     class meta:
         model = Portada
 
@@ -21,5 +22,7 @@ class BotAplicationAdmin(admin.ModelAdmin):
     list_display = ('name','endpoint','width','height','updated','created',)
     list_editable = ('endpoint',)
 
+
+# admin.site.register(Noticia)
 admin.site.register(BotAplication,BotAplicationAdmin)
 admin.site.register(Portada, PortadaAdmin)
